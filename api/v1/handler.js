d0fd63bd431b0bab9b176953e5b9f6d38b7f95a6
Juha Räisälä <juha.raisala@gmail.com>
2019-02-20 09:34:29 +0200

    Add request.query.count to handler

--- api/v1/handler.js
+++ api/v1/handler.js
@@ -8,15 +8,26 @@ const Handler = {
 	RssToJson: {
 	  handler: async (request, h) => {
 
+		const rss_count = request.query.count
+
 		const rss_url = request.query.rss_url
-
-		console.log(rss_url)
 		
-		let feed = await parser.parseURL(rss_url);
+		async function getFeed(){
+			let feed = await parser.parseURL(rss_url);
 
-		return h.response(feed)
-	  },
-	  tags: ['api', 'RSS TO JSON']
+			let feed2 = feed.items.slice(0, rss_count);
+
+			return feed2
+		}
+
+	
+		let myFeed = await getFeed();
+
+		console.log(myFeed)
+
+
+		return h.response(myFeed)
+	  }
 	}
 }
 
